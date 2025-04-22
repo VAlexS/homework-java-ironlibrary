e1cbca31e556ffc48c0ccd66ef2a7ed46e5f81c7
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-22 15:31:22 +0200

    Commit inicial

---
+++ src/main/java/com/example/homework_libraty/HomeworkLibratyApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_libraty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkLibratyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkLibratyApplication.class, args);
+	}
+
+}
