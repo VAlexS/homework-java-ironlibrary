007ab79da5ce820e4acb1a9a067d1fb9339d7c68
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-22 17:56:33 +0200

    Aplicacion prueba

--- src/main/java/com/example/homework_libraty/repositories/BookRepository.java
+++ src/main/java/com/example/homework_libraty/repositories/BookRepository.java
@@ -2,6 +2,10 @@ package com.example.homework_libraty.repositories;
 
 import com.example.homework_libraty.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    Book findByTitle(String tittle);
 }
